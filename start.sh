12b81d8fd96341113fbba36da872c6cfaaaa46b7
Li Xin <i@crzidea.com>
2025-02-21 20:19:03 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 # https://stackoverflow.com/questions/600079/how-do-i-clone-a-subdirectory-only-of-a-git-repository/52269934#52269934
 # git clone -n --depth=1 --filter=tree:0 https://github.com/crzidea/docker-start.git
 # chmod +x docker-start/start.sh
-# docker-start/start.sh comfyui-hunyuan-video
+# docker-start/start.sh comfyui-SkyReels-Hunyuan-I2V
 
 cd `pwd`
 
