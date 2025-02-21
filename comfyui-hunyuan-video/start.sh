eb192a6149be12ced3460263bf749d37aea4a40e
Crzidea <i@crzidea.com>
2025-02-21 19:50:04 +0800

    Update mode

--- comfyui-hunyuan-video/start.sh
+++ comfyui-hunyuan-video/start.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # bash <(curl -s https://gist.githubusercontent.com/crzidea/e9f7e6ff1f53761d3377c24d1ae71e43/raw/before-entrypoint.sh)
 # curl -L -o /runner-scripts/download-models.txt https://gist.githubusercontent.com/crzidea/e9f7e6ff1f53761d3377c24d1ae71e43/raw/download-models.txt
 # curl -L -o /runner-scripts/prestart.sh https://gist.githubusercontent.com/crzidea/e9f7e6ff1f53761d3377c24d1ae71e43/raw/prestart.sh
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # https://stackoverflow.com/questions/600079/how-do-i-clone-a-subdirectory-only-of-a-git-repository/52269934#52269934
 # git clone -n --depth=1 --filter=tree:0 https://github.com/crzidea/docker-start.git
 # chmod +x docker-start/start.sh
