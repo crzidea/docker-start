b50ed0a38644731c8141fa3862206569b3a6a655
Li Xin <i@crzidea.com>
2025-02-21 18:02:02 +0800

    Update before-entrypoint.sh

--- comfyui-hunyuan-video/before-entrypoint.sh
+++ comfyui-hunyuan-video/before-entrypoint.sh
@@ -1,3 +1,4 @@
 # bash <(curl -s https://gist.githubusercontent.com/crzidea/e9f7e6ff1f53761d3377c24d1ae71e43/raw/before-entrypoint.sh)
-curl -L -o /runner-scripts/download-models.txt https://gist.githubusercontent.com/crzidea/e9f7e6ff1f53761d3377c24d1ae71e43/raw/download-models.txt
-curl -L -o /runner-scripts/prestart.sh https://gist.githubusercontent.com/crzidea/e9f7e6ff1f53761d3377c24d1ae71e43/raw/prestart.sh
+# curl -L -o /runner-scripts/download-models.txt https://gist.githubusercontent.com/crzidea/e9f7e6ff1f53761d3377c24d1ae71e43/raw/download-models.txt
+# curl -L -o /runner-scripts/prestart.sh https://gist.githubusercontent.com/crzidea/e9f7e6ff1f53761d3377c24d1ae71e43/raw/prestart.sh
+mv download-models.txt prestart.sh /runner-scripts/
