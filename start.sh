6b65ec6ba52f2c817f650b87a9b66b6e96bb7bc7
Li Xin <i@crzidea.com>
2025-02-21 19:46:01 +0800

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+# https://stackoverflow.com/questions/600079/how-do-i-clone-a-subdirectory-only-of-a-git-repository/52269934#52269934
+# git clone -n --depth=1 --filter=tree:0 https://github.com/crzidea/docker-start.git
+# chmod +x docker-start/start.sh
+# docker-start/start.sh comfyui-hunyuan-video
+cd `pwd`
+git sparse-checkout set --no-cone /$1
+git checkout $1
+cd $1
+./start.sh
