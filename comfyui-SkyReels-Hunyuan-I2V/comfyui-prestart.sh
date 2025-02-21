3f5a9dcd01d58a98cf536590e93c7431ff5c7aac
Crzidea <i@crzidea.com>
2025-02-21 20:17:05 +0800

    Add comfyui-SkyReels-Hunyuan-I2V

---
+++ comfyui-SkyReels-Hunyuan-I2V/comfyui-prestart.sh
@@ -0,0 +1,5 @@
+# Download workflow
+mkdir -p /root/ComfyUI/user/default/workflows
+curl -L -o \
+  /root/ComfyUI/user/default/workflows/skyreels_hunyuan_I2V_native_example_01.json \
+  https://huggingface.co/Kijai/SkyReels-V1-Hunyuan_comfy/resolve/main/skyreels_hunyuan_I2V_native_example_01.json
