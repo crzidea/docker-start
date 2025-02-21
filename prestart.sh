abe2f2d05c2293a0a1d7601e83e6c30695c301b3
Li Xin <i@crzidea.com>
2025-02-21 18:00:56 +0800

    Delete prestart.sh

--- prestart.sh
+++
@@ -1,5 +0,0 @@
-# Download workflow
-# https://comfyanonymous.github.io/ComfyUI_examples/hunyuan_video/hunyuan_video_text_to_video.json
-# ComfyUI/user/default/workflows/hunyuan_video_text_to_video.json
-mkdir -p /root/ComfyUI/user/default/workflows
-curl -L -o /root/ComfyUI/user/default/workflows/hunyuan_video_text_to_video.json https://comfyanonymous.github.io/ComfyUI_examples/hunyuan_video/hunyuan_video_text_to_video.json
