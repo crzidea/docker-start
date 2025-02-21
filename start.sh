a7d000860d1103ad1f5c4b5eb1e462be90bd195b
Li Xin <i@crzidea.com>
2025-02-21 20:34:01 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-# https://stackoverflow.com/questions/600079/how-do-i-clone-a-subdirectory-only-of-a-git-repository/52269934#52269934
-# git clone -n --depth=1 --filter=tree:0 https://github.com/crzidea/docker-start.git
-# chmod +x docker-start/start.sh
-# docker-start/start.sh comfyui-SkyReels-Hunyuan-I2V
+# bash <(curl -s https://raw.githubusercontent.com/crzidea/docker-start/refs/heads/main/start.sh) comfyui-SkyReels-Hunyuan-I2V
 
-cd `pwd`
+# https://stackoverflow.com/questions/600079/how-do-i-clone-a-subdirectory-only-of-a-git-repository/52269934#52269934
+git clone -n --depth=1 --filter=tree:0 https://github.com/crzidea/docker-start.git
+
+cd docker-start
 
 if [ ! -f ".install-complete" ] ; then
   git sparse-checkout set --no-cone /$1
