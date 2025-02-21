664a19043b680e0e474521323156777088a3d3bf
Li Xin <i@crzidea.com>
2025-02-21 19:56:17 +0800

    Update start.sh

--- comfyui-hunyuan-video/start.sh
+++ comfyui-hunyuan-video/start.sh
@@ -3,18 +3,22 @@
 # curl -L -o /runner-scripts/download-models.txt https://gist.githubusercontent.com/crzidea/e9f7e6ff1f53761d3377c24d1ae71e43/raw/download-models.txt
 # curl -L -o /runner-scripts/prestart.sh https://gist.githubusercontent.com/crzidea/e9f7e6ff1f53761d3377c24d1ae71e43/raw/prestart.sh
 
-curl -L -o cloudflared https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
-chmod +x cloudflared
+if [ ! -f ".install-complete" ] ; then
+	curl -L -o cloudflared https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+  chmod +x cloudflared
+  pip install jupyterlab
+  mv download-models.txt prestart.sh /runner-scripts/
+  
+  touch .download-complete
+fi ;
+
 ./cloudflared tunnel run --token $CLOUDFLARED_TOKEN 2>&1 | tee -a cloudflared.log &
 
-pip install jupyterlab
 jupyter lab --no-browser --allow-root --port=8888 \
 	--ServerApp.token=$MY_JUPYTER_TOKEN \
 	--ServerApp.preferred_dir=/ \
 	--ServerApp.root_dir=/ \
 	--ServerApp.allow_remote_access=true &
-
-mv download-models.txt prestart.sh /runner-scripts/
 
 chmod +x /runner-scripts/entrypoint.sh
 /runner-scripts/entrypoint.sh
