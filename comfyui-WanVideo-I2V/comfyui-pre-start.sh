a902f5d89682b3746ec31b5e845ff4b6582343ef
Crzidea <i@crzidea.com>
2025-02-28 14:47:06 +0800

    Fix path

--- comfyui-WanVideo-I2V/comfyui-pre-start.sh
+++ comfyui-WanVideo-I2V/comfyui-pre-start.sh
@@ -9,12 +9,15 @@
 # Install custom_nodes
 
 if [ ! -f ".custom-nodes-installed" ] ; then
-  cd /root/ComfyUI/custom_nodes
+  pushd /root/ComfyUI/custom_nodes
 
   # ComfyUI-WanVideoWrapper
   git clone https://github.com/kijai/ComfyUI-WanVideoWrapper.git
-  cd ComfyUI-WanVideoWrapper
+  pushd ComfyUI-WanVideoWrapper
   pip install -r requirements.txt
+
+  popd
+  popd
 
   touch .custom-nodes-installed
 fi ;
