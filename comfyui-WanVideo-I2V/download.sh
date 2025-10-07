133d28c53bb77cfff23ed3139c8b678c5032ef90
Crzidea <i@crzidea.com>
2025-10-07 12:35:36 +0800

    feat: Add download script for models
    This script will download the models listed in `download-models.txt` using `aria2c`.

---
+++ comfyui-WanVideo-I2V/download.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "########################################"
+echo "[INFO] Downloading Models..."
+echo "########################################"
+
+# Models
+cd /root/ComfyUI/models
+aria2c \
+  --input-file=/runner-scripts/download-models.txt \
+  --allow-overwrite=false \
+  --auto-file-renaming=false \
+  --continue=true \
+  --max-connection-per-server=5
+
+# Finish
+touch /root/.download-complete
