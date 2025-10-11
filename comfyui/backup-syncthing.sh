522faebb833426bb006bf980c68acc3e6a7d995c
Crzidea <i@crzidea.com>
2025-10-12 01:14:17 +0800

    refactor: Use temporary directory for backup files in backup-syncthing.sh

--- comfyui/backup-syncthing.sh
+++ comfyui/backup-syncthing.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd ~/.local/state/syncthing
-mkdir ../tmp/syncthing
-cp config.xml cert.pem key.pem https-cert.pem https-key.pem ../tmp/syncthing/
-cd ../tmp
+TEMP_DIR=$(mktemp -d)
+mkdir $TEMP_DIR/syncthing
+cp config.xml cert.pem key.pem https-cert.pem https-key.pem $TEMP_DIR/syncthing/
+cd $TEMP_DIR/
 tar czf syncthing.tar.gz syncthing
 base64 -w 0 syncthing.tar.gz
 echo
+rm -rf $TEMP_DIR
