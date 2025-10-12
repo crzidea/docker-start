429af00fac1ee6e7c60a963943529aa9a100684b
Crzidea <i@crzidea.com>
2025-10-12 12:15:07 +0800

    feat: Add backup and update scripts for Syncthing configuration

---
+++ comfyui/syncthing-update-raw-listen-address.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./syncthing/syncthing cli config options raw-listen-addresses 0 delete
+./syncthing/syncthing cli config options raw-listen-addresses 0 delete
+./syncthing/syncthing cli config options raw-listen-addresses add tcp://:22000
+./syncthing/syncthing cli config options raw-listen-addresses add tcp://:$QUICKPOD_PORT_22000
