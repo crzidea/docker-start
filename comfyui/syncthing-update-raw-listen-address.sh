a5e1163adba11ed5ab888734c1ca0844fbcb2f1e
Crzidea <i@crzidea.com>
2025-10-12 12:28:19 +0800

    fix: Add missing cd command to ensure script runs in the correct directory

--- comfyui/syncthing-update-raw-listen-address.sh
+++ comfyui/syncthing-update-raw-listen-address.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd
 ./syncthing/syncthing cli config options raw-listen-addresses 0 delete
 ./syncthing/syncthing cli config options raw-listen-addresses 0 delete
 ./syncthing/syncthing cli config options raw-listen-addresses add tcp://:22000
